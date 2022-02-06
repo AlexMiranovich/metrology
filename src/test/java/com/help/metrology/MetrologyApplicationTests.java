7a940a7ebf5d28fdf27516edb8628d28605f3358
Alex Miranovich <mir92on@gmail.com>
2022-02-06 13:55:35 +0300

    init commit

---
+++ src/test/java/com/help/metrology/MetrologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.help.metrology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetrologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
